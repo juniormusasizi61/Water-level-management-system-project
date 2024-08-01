6116973bf49bc5dc79c56b87b1b9a1051e908283
blaxpyda <arindaj33@gmail.com>
2024-08-01 05:30:26 +0300

    set up

--- diesel.c
+++ diesel.c
@@ -5,3 +5,28 @@ int calculateWaterLevel(int distance) {
 }
 
 //initialize your vars
+void setup() {
+  // Initialize serial communication
+  Serial.begin(115200);
+  mySerial.begin(9600);
+
+  // Initialize LCD
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+
+  // Set pin modes
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(yellowLedPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+  // Turn off LEDs and buzzer
+  digitalWrite(greenLedPin, LOW);
+  digitalWrite(yellowLedPin, LOW);
+  digitalWrite(redLedPin, LOW);
+  digitalWrite(buzzerPin, LOW);
+
+  Serial.println("Setup complete");

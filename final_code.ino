7dd950c179d2b60afd3dc9280e55d2e180cd44fd
@Alidat <alvinnahereza@gmail.com>
2024-08-02 16:28:13 +0300

    final

--- final_code.ino
+++ final_code.ino
@@ -22,3 +22,6 @@ IPAddress subnet(255, 255, 255, 0);
 WiFiClient client;
 
 WebServer server(80);
+
+// Initialize the LCD with I2C address 0x27 and 16x2 display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
